fca34af3607fdd61be08ad7a4c0ce18299efa1e1
David Hilera <david.hilera@hotmail.com>
2024-08-30 20:06:59 -0600

    style:art: Remove unused elements from EmptyPage
    Removed unused product-related elements and adjusted layout padding in the EmptyPage component for a cleaner design. Updated Footer component to align social media icons properly.
    
    - Removed product grid and button from EmptyPage.
    - Adjusted alignment of social media icons in Footer component.

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,11 +1,7 @@
 import Link from 'next/link'
 import { IoCartOutline } from 'react-icons/io5'
-import { getPaginationProducts } from '@/actions'
-import { CurrentProductsGrid, TitleCategory } from '@/components'
-import { Button } from '@/components/ui/button'
 
 export default async function EmptyPage() {
-  const { products } = await getPaginationProducts({ page: 1 })
   return (
     <>
       <div className='flex justify-center items-center h-[400px] mt-[60.67px] pt-10' >
@@ -18,20 +14,6 @@ export default async function EmptyPage() {
             Comprar
           </Link>
         </div>
-      </div>
-
-      <div className='pb-10 mb-10 border-t border-black'>
-        <TitleCategory
-          title="Recomendados para ti"
-          subtitle="Lo más nuevo"
-          className='text-center uppercase pt-10'
-        />
-        <CurrentProductsGrid products={products} />
-        <Button asChild variant='outline' className='flex justify-center mx-auto w-3/4 max-w-32 uppercase bg-transparent mt-[52px]'>
-          <Link href='/products'>
-            ver todo
-          </Link>
-        </Button>
       </div>
     </>
   )
