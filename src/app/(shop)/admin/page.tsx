3813e90f675da603f6b2abc7aa389b246a831f8f
David Hilera <david.hilera@hotmail.com>
2024-09-09 06:10:59 -0600

    refactor: :recycle: Remove unused product selectors
    Removed `QuantitySelector` and `SizeSelector` components as they are no longer being used in the codebase. Updated the component exports to reflect these removals.
    
    This change helps to clean up the codebase by eliminating unused components, improving maintainability and reducing potential confusion.

--- src/app/(shop)/admin/page.tsx
+++ src/app/(shop)/admin/page.tsx
@@ -8,8 +8,8 @@ import { Button } from '@/components/ui/button'
 
 export default function AdminPage() {
   return (
-    <div className="">
-      <div className=''>
+    <div className="@container">
+      <div className="grid grid-cols-1 gap-6 @4xl:grid-cols-2 @7xl:grid-cols-12 3xl:gap-8">
         <WelcomeBanner
           title={
             <>
