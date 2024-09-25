b7de4af2e3370625d797477d44f1d87cad14a85b
David Hilera <david.hilera@hotmail.com>
2024-09-25 05:21:54 -0600

    fix: :bug: correct user count and improve auth layout
    - Fixed incorrect count function from `prisma.order` to `prisma.user` in `getPaginatedUsers`.
    - Increased width of auth layout container from 350px to 500px for better UI.
    - Centered titles in login and new account pages for improved alignment.
    - Added a tagline to the new account page: "Compra rápido y gestiona tus pedidos en un solo lugar."

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col min-h-screen pt-32">
 
-      <h1 className={`${titleFont.className} text-4xl mb-5`}>Ingresar</h1>
+      <h1 className={`${titleFont.className} text-center text-4xl mb-5`}>Ingresar</h1>
 
       <LoginForm />
     </div>
--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -5,8 +5,8 @@ export default function NewAccountPage() {
   return (
     <div className="flex flex-col min-h-screen pt-32">
 
-      <h1 className={`${titleFont.className} text-4xl mb-5`}>Nueva cuenta</h1>
-
+      <h1 className={`${titleFont.className} text-center text-4xl mb-5`}>Nueva cuenta</h1>
+      <p className='text-center pb-10'>Compra rápido y gestiona tus pedidos en un solo lugar.</p>
       <RegisterForm />
 
     </div>
