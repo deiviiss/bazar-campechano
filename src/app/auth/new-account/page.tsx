4205779b1ab2253de3f23182dc22a1570a1a5211
David Hilera <david.hilera@hotmail.com>
2024-12-15 02:56:00 -0600

    refactor: :recycle: Improve auth forms with consistent button styles
    Updated login and registration forms to use the shared `Button`
    component for consistent styling and improved UI coherence.
    
    Changes include:
    - Replaced direct `Link` usage with `Button` wrapping `Link` for "Crear una nueva cuenta" and "Ingresar" actions.
    - Added terms and privacy acknowledgment text with links on the registration page.
    
    This enhances code reusability and provides a unified user experience.

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { RegisterForm } from './ui/RegisterForm'
 import { titleFont } from '@/config/fonts'
 
@@ -8,7 +9,11 @@ export default function NewAccountPage() {
       <h1 className={`${titleFont.className} text-center text-4xl mb-5`}>Nueva cuenta</h1>
       <p className='text-center pb-10'>Compra rápido y gestiona tus pedidos en un solo lugar.</p>
       <RegisterForm />
-
+      <p className="mt-10">
+        <span>
+          Al hacer clic en &quot;Crear cuenta&quot;, aceptas nuestros <Link href="/info/terms" className="underline">términos y condiciones</Link> y <Link href={'/info/privacy'} className='underline'>política de privacidad</Link>
+        </span>
+      </p>
     </div>
   )
 }
