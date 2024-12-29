cf61cb5e613ee66fe02af166afd78970305f38ef
David Hilera <david.hilera@hotmail.com>
2024-12-29 10:17:22 -0600

    style: :art: Fix inconsistent import quotation style in layout
    Updated the quotation style for the `SpeedInsights` import in the layout file to maintain consistency with other imports.
    
    Improves code readability and adheres to style conventions.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Providers } from '@/components'
 import { textFont } from '@/config/fonts'
